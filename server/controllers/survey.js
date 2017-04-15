ace9ad969ac870a5ff2ef2c789bb78b759b163b3
taranjit91 <parmar.taran@gmail.com>
2017-04-15 01:53:56 -0400

    rating route set

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -66,9 +66,9 @@ module.exports.DisplayRatingAnswers = (req, res) => {
   });
 }
 
-// Displays the Survey to take it
+// Displays the short answers Survey to take it
 
-module.exports.TakeSurvey=(req,res) =>
+module.exports.TakeShortAnswersSurvey=(req,res) =>
 {
   try {
       // get a reference to the id from the url
@@ -81,6 +81,33 @@ module.exports.TakeSurvey=(req,res) =>
           res.end(error);
         } else {
         res.render('survey/detailshort', {
+        title: "Survey Name",
+        surveys: survey,
+        displayName: req.user ? req.user.displayName : ''
+  });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    
+    }
+}
+
+// Displays the rating answers Survey to take it
+
+module.exports.TakeRatingSurvey=(req,res) =>
+{
+  try {
+      // get a reference to the id from the url
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        // finding  book by its id
+      survey.findById(id, (err, survey) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+        res.render('survey/detailrating', {
         title: "Survey Name",
         surveys: survey,
         displayName: req.user ? req.user.displayName : ''
